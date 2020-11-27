e52c1ed5c1d6c9375050df690d59863248a729c8
Devon Burnham <novedevo@gmail.com>
2020-11-27 10:01:40 -0800

    modified states

--- final/activation_unit.vhd
+++ final/activation_unit.vhd
@@ -13,7 +13,7 @@ END ENTITY;
 
 ARCHITECTURE structure OF activation_unit IS
 
-SIGNAL state: unsigned (2 DOWNTO 0);
+SIGNAL state: integer range 0 to 9;
 signal sigRow0 : bus_width;
 signal sigRow1 : bus_width;
 signal sigRow2 : bus_width;
@@ -23,29 +23,29 @@ BEGIN
     PROCESS (clk, reset, hard_reset, stall)
     BEGIN
         IF (hard_reset = '1' or reset = '1') THEN
-            state <= to_unsigned(0, 4);
+            state <= 0;
         ELSIF (rising_edge(clk) and stall = '0') THEN
-            state <= state + to_unsigned(1, 4);
+            state <= state + 1;
         else
             state <= state;
         END IF;
     END PROCESS;
 
-    sigRow0(0) <= y_in0 when state = to_unsigned(4, 4);
+    sigRow0(0) <= y_in0 when state = 4;
 
-    sigRow0(1) <= y_in1 when state = to_unsigned(5, 4);
-    sigRow1(0) <= y_in0 when state = to_unsigned(5, 4);
+    sigRow0(1) <= y_in1 when state = 5;
+    sigRow1(0) <= y_in0 when state = 5;
 
-    sigRow0(2) <= y_in2 when state = to_unsigned(6, 4);
-    sigRow1(1) <= y_in1 when state = to_unsigned(6, 4);
-    sigRow2(0) <= y_in0 when state = to_unsigned(6, 4);
+    sigRow0(2) <= y_in2 when state = 6;
+    sigRow1(1) <= y_in1 when state = 6;
+    sigRow2(0) <= y_in0 when state = 6;
 
-    sigRow1(2) <= y_in2 when state = to_unsigned(7, 4);
-    sigRow2(1) <= y_in1 when state = to_unsigned(7, 4);
+    sigRow1(2) <= y_in2 when state = 7;
+    sigRow2(1) <= y_in1 when state = 7;
 
-    sigRow2(2) <= y_in0 when state = to_unsigned(8,4);
+    sigRow2(2) <= y_in0 when state = 8;
 
-    done <= '1' when state = to_unsigned(8,4);
+    done <= '1' when state = 9;
 
     row0 <= sigRow0;
     row1 <= sigRow1;
