b623d29c9c15c13a5f28016c8e58692c972005c8
Devon Burnham <novedevo@gmail.com>
2020-12-01 13:04:03 -0800

    more stpu work

--- final/stpu.vhd
+++ final/stpu.vhd
@@ -55,15 +55,25 @@ ARCHITECTURE structure OF stpu IS
     END COMPONENT;
 
     --signals
+
+    SIGNAL mode : t_MODE_STATE;
+    SIGNAL loaded_weight : t_WEIGHT_STATE;
+
     SIGNAL donesig, mmuLdSig, mmuLdWSig : STD_LOGIC;
     SIGNAL y0sig, y1sig, y2sig, w0sig, w1sig, w2sig, a0sig, a1sig, a2sig : unsigned(7 DOWNTO 0);
 
-    signal uclr0, uclr1, uclr2, urden0, urden1, urden2, uwren0, uwren1, uwren2, wclr, wrden, wwren : STD_LOGIC;
-    signal uaddr0, uaddr1, uaddr2, waddr : STD_LOGIC_VECTOR(1 DOWNTO 0);
-    signal udata0, udata1, udata2, uq1, uq2, uq0 : STD_LOGIC_VECTOR(7 downto 0);
-    signal wdata, w1 : STD_LOGIC_VECTOR(23 DOWNTO 0);
+    SIGNAL resetSig : STD_LOGIC;
+
+    --consider setting all these to 0 initially
+    signal uwren, urden : std_logic_vector(2 downto 0);
+    SIGNAL wrden, wwren : STD_LOGIC;
+    SIGNAL uaddr0, uaddr1, uaddr2, waddr : STD_LOGIC_VECTOR(1 DOWNTO 0);
+    SIGNAL udata0, udata1, udata2, uq1, uq2, uq0 : STD_LOGIC_VECTOR(7 DOWNTO 0);
+    SIGNAL wdata, w1 : STD_LOGIC_VECTOR(23 DOWNTO 0);
 
 BEGIN
+
+    resetSig <= reset OR hard_reset;
 
     --instantiate components
     au : activation_unit
@@ -73,13 +83,45 @@ BEGIN
     PORT MAP(clk, reset, hard_reset, mmuLdSig, mmuLdWSig, stall, a0sig, a1sig, a2sig, w0sig, w1sig, w2sig, y0sig, y1sig, y2sig);
 
     u0 : uram
-    PORT MAP(uclr0, uaddr0, clk, udata0, urden0, uwren0, uq0);
+    PORT MAP(resetSig, uaddr0, clk, a_in(7 downto 0), urden(0), uwren(0), uq0);
     u1 : uram
-    PORT MAP(uclr1, uaddr1, clk, udata1, urden1, uwren1, uq1);
+    PORT MAP(resetSig, uaddr1, clk, a_in(15 downto 8), urden(1), uwren(1), uq1);
     u2 : uram
-    PORT MAP(uclr2, uaddr2, clk, udata2, urden2, uwren2, uq2);
+    PORT MAP(resetSig, uaddr2, clk, a_in(23 downto 16), urden(2), uwren(2), uq2);
 
     w : wram
-    PORT MAP(wclr, waddr, clk, wdata, wrden, wwren, wq);
+    PORT MAP(hard_reset, waddr, clk, weights, wrden, wwren, wq);
+
+    PROCESS (clk, reset, hard_reset) IS
+    BEGIN
+        IF (hard_reset = '1') THEN
+            loaded_weight <= none;
+            mode <= idle;
+        ELSIF (reset = '1') THEN
+            mode <= idle;
+
+        ELSIF (falling_edge(clk)) THEN --falling edge to align it better with the sram
+
+            IF (setup = '1' AND (mode = idle OR mode = go)) THEN
+                mode <= setup;
+                wwren <= '1';
+                loaded_weight <= first;
+                waddr <= "00";
+
+                uwren <= "111";
+                udata0 <= a_in(7 downto 0);
+
+            elsif (mode = setup and loaded_weight = first) then
+                waddr <= "01";
+                loaded_weight <= second;
+            elsif (mode = setup and loaded_weight = second) then
+                waddr <= "10";
+                loaded_weight <= done;
+            END IF;
+
+
+
+        END IF;
+    END PROCESS;
 
 END structure;
--- final/systolic_package.vhd
+++ final/systolic_package.vhd
@@ -7,6 +7,8 @@ PACKAGE systolic_package IS
     CONSTANT N : INTEGER := 8;
     TYPE bus_width IS ARRAY (0 TO 2) OF UNSIGNED(N - 1 DOWNTO 0);
     TYPE t_MMU_STATE IS (idle, load_col0, load_col1, load_col2);
+    type t_MODE_STATE is (idle, setup, go);
+    type t_WEIGHT_STATE is (none, first, second, done);
 END PACKAGE systolic_package;
 
 PACKAGE BODY systolic_package IS
